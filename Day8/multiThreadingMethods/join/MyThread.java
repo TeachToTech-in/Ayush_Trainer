b126730b80c67797af0b89e200cfe3ed8c1e7c59
ayushkt7 <9771504025a@>
2025-09-02 01:36:43 +0530

    Day 10 commit

--- Day8/multiThreadingMethods/join/MyThread.java
+++ Day8/multiThreadingMethods/join/MyThread.java
@@ -1,15 +1,11 @@
-package Day8.multiThreadingMethods.yeild;
+package Day8.multiThreadingMethods.join;
 
 public class MyThread implements Runnable {
 
     @Override
     public void run() {
-        Thread t = Thread.currentThread();
         for (int i = 0; i < 5; i++)
             System.out.println("Mythread class");
-        Thread.yield();
-
-        //Since always it is calling yeild method it means it will always give chance to other threds first i.e main thread here
 
     }
 
