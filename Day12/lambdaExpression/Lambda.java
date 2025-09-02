f60d7da8e4a252d396f1f8f86f487a1a8aac69f6
ayushkt7 <9771504025a@>
2025-09-02 20:42:00 +0530

    lambda

---
+++ Day12/lambdaExpression/Lambda.java
@@ -0,0 +1,19 @@
+package Day12.lambdaExpression;
+
+public class Lambda {
+
+    public static void main(String[] args) {
+            Speaker sony = (singer) -> {
+
+            {
+                System.out.println("-_-_-_-_-***PLAYING " +singer+"***-_-_-_-_-");
+            }
+            
+        };
+
+        sony.play("ARIJIT");
+    }
+    
+}
+
+
---
+++ Day12/lambdaExpression/Speaker.java
@@ -0,0 +1,5 @@
+package Day12.lambdaExpression;
+
+public interface Speaker {
+        void play(String artist);
+}
