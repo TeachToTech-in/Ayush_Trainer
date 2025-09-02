92fd3d558c688c4d5572733d34e0510cb00b5a3a
ayushkt7 <9771504025a@>
2025-09-02 21:44:04 +0530

    lambda

--- Day12/lambdaExpression/Lambda.java
+++ Day12/lambdaExpression/Lambda.java
@@ -3,17 +3,22 @@ package Day12.lambdaExpression;
 public class Lambda {
 
     public static void main(String[] args) {
-            Speaker sony = (singer) -> {
-
-            {
-                System.out.println("-_-_-_-_-***PLAYING " +singer+"***-_-_-_-_-");
-            }
-            
-        };
-
+    
+        Speaker sony = singer -> System.out.println("-_-_-_-_-***PLAYING " +singer+"***-_-_-_-_-");
+                        
         sony.play("ARIJIT");
     }
+
     
 }
+
+ interface Speaker {
+        void play(String artist);
+}
+
+
+
+
+
 
 
--- Day12/lambdaExpression/Speaker.java
+++ Day12/lambdaExpression/Speaker.java
@@ -1,5 +1,6 @@
 package Day12.lambdaExpression;
 
+@FunctionalInterface
 public interface Speaker {
         void play(String artist);
 }
